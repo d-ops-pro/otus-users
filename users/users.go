6e23fcf5263c70db5aab8c992b0ce2688fa139f1
Dmitry Ivanov <me@d-ops.pro>
2021-05-05 08:18:14 +0300

    Add operations logging for the users CRUD

--- users/users.go
+++ users/users.go
@@ -22,16 +22,78 @@ func ConfigureRouter(r *mux.Router, db *gorm.DB) {
 
 func CreateHandler(db *gorm.DB) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		user := new(domain.User)
+		logger := logrus.WithField("operation", "user_create")
 
-		err := json.NewDecoder(r.Body).Decode(user)
+		user := new(domain.User)
+		{
+			logger.Infof("decoding request body")
+			err := json.NewDecoder(r.Body).Decode(user)
+			if err != nil {
+				logger.WithError(err).Error("unable to decode the request body")
+				w.WriteHeader(http.StatusBadRequest)
+				_, _ = w.Write([]byte("unable to decode the request body"))
+				return
+			}
+		}
+
+		logger.Infof("creating the user entity: %+v", user)
+		err := db.Create(user).Error
 		if err != nil {
+			logger.WithError(err).Error("failed to create the user entity")
+			w.WriteHeader(http.StatusInternalServerError)
+			_, _ = w.Write([]byte("unable to save the user"))
+			return
+		}
+
+		logger.Infof("user successfully created with id: %d", user.ID)
+		w.WriteHeader(http.StatusOK)
+		_ = json.NewEncoder(w).Encode(user)
+	}
+}
+
+func UpdateHandler(db *gorm.DB) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		vars := mux.Vars(r)
+		idRaw := vars["id"]
+		logger := logrus.WithField("operation", "user_update").WithField("user_id", idRaw)
+
+		logger.Infof("parsing the user id as INT")
+		id, err := strconv.Atoi(idRaw)
+		if err != nil {
+			logger.WithError(err).Error("failed to parse the user id to int")
+			w.WriteHeader(http.StatusBadRequest)
+			_, _ = w.Write([]byte("provided id is not an int type"))
+			return
+		}
+
+		user := &domain.User{ID: id}
+		logger.Infof("checking the user exists by provided id")
+		err = db.Take(user, "id=?", id).Error
+		if err != nil {
+			logger.WithError(err).Error("unable to find the user by provided id")
+
+			if errors.Is(err, gorm.ErrRecordNotFound) {
+				w.WriteHeader(http.StatusNotFound)
+				_, _ = w.Write([]byte("user not found \n"))
+				return
+			}
+
+			w.WriteHeader(http.StatusInternalServerError)
+			_, _ = w.Write([]byte("internal server error \n"))
+			return
+		}
+
+		logger.Info("user for update found")
+		err = json.NewDecoder(r.Body).Decode(user)
+		if err != nil {
+			logger.WithError(err).Error("unable to decode the request body")
 			w.WriteHeader(http.StatusBadRequest)
 			_, _ = w.Write([]byte("unable to decode the request body"))
 			return
 		}
 
-		err = db.Create(user).Error
+		logger.Infof("updating the user with a new data: %+v", user)
+		err = db.Save(user).Error
 		if err != nil {
 			w.WriteHeader(http.StatusInternalServerError)
 			_, _ = w.Write([]byte("unable to save the user"))
@@ -43,81 +105,54 @@ func CreateHandler(db *gorm.DB) http.HandlerFunc {
 	}
 }
 
-func UpdateHandler(db *gorm.DB) http.HandlerFunc {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		vars := mux.Vars(r)
-		idRaw := vars["id"]
-
-		id, err := strconv.Atoi(idRaw)
-		if err != nil {
-			logrus.WithError(err).Error("failed to parse the user id to int")
-			w.WriteHeader(http.StatusBadRequest)
-			_, _ = w.Write([]byte("provided id is not an int type"))
-			return
-		}
-
-		user := &domain.User{ID: id}
-		err = json.NewDecoder(r.Body).Decode(user)
-		if err != nil {
-			w.WriteHeader(http.StatusBadRequest)
-			_, _ = w.Write([]byte("unable to decode the request body"))
-			return
-		}
-
-		err = db.Save(user).Error
-		if err != nil {
-			w.WriteHeader(http.StatusInternalServerError)
-			_, _ = w.Write([]byte("unable to save the user"))
-			return
-		}
-
-		w.WriteHeader(http.StatusOK)
-		_ = json.NewEncoder(w).Encode(user)
-	})
-}
-
 func GetHandler(db *gorm.DB) http.HandlerFunc {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
 		vars := mux.Vars(r)
 		idRaw := vars["id"]
+		logger := logrus.WithField("operation", "user_get").WithField("user_id", idRaw)
 
+		logger.Infof("parsing the user id as INT")
 		id, err := strconv.Atoi(idRaw)
 		if err != nil {
-			logrus.WithError(err).Error("failed to parse the user id to int")
+			logger.WithError(err).Error("failed to parse the user id to int")
 			w.WriteHeader(http.StatusBadRequest)
 			_, _ = w.Write([]byte("provided id is not an int type"))
 			return
 		}
 
+		logger.Infof("searching the user")
 		user := new(domain.User)
 		err = db.Take(user, "id=?", id).Error
 		if err != nil {
+			logger.WithError(err).Error("unable to get the user by ID")
 			if errors.Is(err, gorm.ErrRecordNotFound) {
 				w.WriteHeader(http.StatusNotFound)
 				_, _ = w.Write([]byte("user not found"))
 				return
 			}
 
-			logrus.WithError(err).Error("failed to get the user by id")
 			w.WriteHeader(http.StatusInternalServerError)
 			return
 		}
 
+		logger.Info("user successfully found by id")
 		err = json.NewEncoder(w).Encode(user)
 		if err != nil {
 			logrus.WithError(err).Error("unable to encode the user")
 		}
-	})
+	}
 }
 
 func DeleteHandler(db *gorm.DB) http.HandlerFunc {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
 		vars := mux.Vars(r)
 		idRaw := vars["id"]
+		logger := logrus.WithField("operation", "user_delete").WithField("user_id", idRaw)
 
+		logger.Info("parsing the user ID as INT")
 		id, err := strconv.Atoi(idRaw)
 		if err != nil {
-			logrus.WithError(err).Error("failed to parse the user id to int")
+			logger.WithError(err).Error("failed to parse the user id to int")
 			w.WriteHeader(http.StatusBadRequest)
 			_, _ = w.Write([]byte("provided id is not an int type"))
 			return
@@ -125,11 +160,12 @@ func DeleteHandler(db *gorm.DB) http.HandlerFunc {
 
 		err = db.Delete(domain.User{}, "id=?", id).Error
 		if err != nil {
-			logrus.WithError(err).Error("unable to delete the user")
+			logger.WithError(err).Error("unable to delete the user")
 			w.WriteHeader(http.StatusInternalServerError)
 			return
 		}
 
+		logger.Info("user successfully deleted")
 		w.WriteHeader(http.StatusOK)
-	})
+	}
 }
